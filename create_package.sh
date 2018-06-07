96471330750d25efc8635d5bb1e589b60b940538
Michael Salter-Townshend <michael.salter-townshend@ucd.ie>
2018-06-07 16:30:48 +0100

    changed from --library to -l for compatibility

--- create_package.sh
+++ create_package.sh
@@ -7,5 +7,5 @@ rlib=$1
 rm -rf mosaicpackage/ mosaicpackage_1.0.tar.gz 
 Rscript create_package.R
 R CMD build mosaicpackage
-R CMD INSTALL --library $rlib mosaicpackage_1.0.tar.gz
+R CMD INSTALL -l $rlib mosaicpackage_1.0.tar.gz
 
