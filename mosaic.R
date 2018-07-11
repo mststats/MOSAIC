501371a64a97b197be9aca1d7cbc39c086b46a03
Michael Salter-Townshend <michael.salter-townshend@ucd.ie>
2018-07-11 12:52:13 +0100

    re-jigging arguments

--- mosaic.R
+++ mosaic.R
@@ -1,25 +1,32 @@
 # script to run all code required to fit MOSAIC. Reads in data, initialises, performs thin->phase->EM cycles and outputs results.
-# example usage: Rscript mosaic.R SanKhomani HGDP/ 4 1 60 16
+# example usage: Rscript mosaic.R Moroccan example_data/ 2 1 2 2 20:22
+# second example: Rscript mosaic.R simulated example_data/ 
 require(MOSAIC)
 ######################## first set some options ###############################
+# FLAG use - flags for arguments
 # important things to set
-shargs<-commandArgs(trailingOnly=TRUE) # read in arguments from the command line; 
+shargs=commandArgs(trailingOnly=TRUE) # read in arguments from the command line; 
 target=shargs[1]  # e.g. Moroccan
 datasource=shargs[2]; # e.g. example_data/ ; note the / at the end is crucial
 L=as.integer(shargs[3]) # number of admixing groups
-firstind<-as.integer(shargs[4]); # which target individual to start from. If NUMA=2 then only this ind is run
+firstind=as.integer(shargs[4]); # which target individual to start from. If NUMA=2 then only this ind is run
 NUMA=as.integer(shargs[5]) # total number of target admixed haplotypes 
+# optional list of chromosomes on which to run mosaic; defaults to 1:22 if not supplied
+chrnos=shargs[6]
+if (!is.na(chrnos)) {chrnos=strsplit(chrnos,":")[[1]];chrnos=as.integer(chrnos[1]):as.integer(chrnos[2])}
+if (is.na(chrnos)) chrnos=1:22 # which chromosomes to run on
+MC=as.integer(shargs[7]) # optional number of cores to use for parallelized code; will grab half of all available if not supplied
+ANC=NULL; # no a-priori knowledge of which panels to use for which ancestries
 
-MC=as.integer(shargs[6]) # optional number of cores to use for parallelized code; will grab half of all available if not supplied
-chrnos=1:22 # which chromosomes to run on
-
-#chrnos=20:22;firstind=1;NUMA=2;L=2;datasource="example_data/";target="Moroccan";ANC=NULL
+#comment this out for demo
+#chrnos=21:22;firstind=1;NUMA=2;L=2;datasource="example_data/";target="Moroccan";ANC=NULL
+#comment this out for simulated data demo
+chrnos=21:22;firstind=1;NUMA=2;L=2;datasource="example_data/";target="simulated";ANC=TRUE
 
 nchrno=length(chrnos) # number of chromosomes for these target haplotypes
 ffpath="/dev/shm/" # location of fast-files
 
 # the rest are mostly used in debugging, etc
-#ANC=NULL; # no a-priori knowledge of which panels to use for which ancestries
 verbose=T # print certain statements of progress as algorithm runs?
 EM=T # run EM algorithm?
 doMu=T # update copying matrix parameters?
@@ -27,7 +34,7 @@ doPI=T # update ancestry switching parameters parameters?
 dorho=T # update recombination w/in same ancestry parameters? 
 dotheta=T # update error / mutation parameters?
 PLOT=F
-return.res=TRUE # whether to return results in a list; for use within an interactive R session
+return.res=interactive() # whether to return results in a list; for use within an interactive R session
 
 # this function includes saving results to disk
 mosaic.result=run_mosaic(ANC,chrnos,datasource,doMu,doPI,dorho,dotheta,EM,ffpath,firstind,
