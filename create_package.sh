9e2a6adf69739c2e82a361ba6d1cd51b2db65d5e
Michael Salter-Townshend <michael.salter-townshend@ucd.ie>
2019-06-05 16:54:06 +0100

    creating vignette to replace tex/pdf

--- create_package.sh
+++ create_package.sh
@@ -20,6 +20,8 @@ echo "importFrom(combinat,permn)" >> MOSAIC/NAMESPACE
 #echo "importFrom(argparser,parse_args)" >> MOSAIC/NAMESPACE
 cp DESCRIPTION MOSAIC/DESCRIPTION
 cp -r inst MOSAIC/
+cp mosaic.Rmd mosaic.pdf vignettes/
+cp -r vignettes/ MOSAIC/
 cp -r man MOSAIC/
 cp MOSAIC-package.Rd MOSAIC/man/
 
@@ -29,4 +31,6 @@ rm MOSAIC/Read-and-delete-me
 
 R CMD build MOSAIC
 R CMD INSTALL -l $rlib MOSAIC_1.3.tar.gz
+
+
 
