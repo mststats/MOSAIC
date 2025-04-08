d1120d1dfa93db7a6d374006999a0f06245ebf15
Michael Salter-Townshend <michael.salter-townshend@ucd.ie>
2025-04-08 16:25:56 +0100

    another update to convert_to_haps.R to facilitate simulations for comparing localanc methods

--- convert_to_haps.R
+++ convert_to_haps.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 # reads in MOSAIC haps files (one per population per chromosome) and outputs .haps files (one per chromosome) 
 require(argparser, quiet=TRUE) 
+require(LaF, quiet=TRUE) 
 m.args=arg_parser("script to convert MOSAIC input files to shapeit2 .haps files")
 ######################## required arguments ###############################
 m.args=add_argument(m.args, "pathin", help="full path to MOSAIC inputs", type="character")
@@ -34,12 +35,17 @@ S=nrow(snps)
 # format of .haps files is RSID RSID position ref alt haps
 Y=matrix(NaN,S,5+NN)
 Y[,1]=chrno;Y[,2]=snps[,1];Y[,3]=snps[,4];Y[,4]=snps[,5];Y[,5]=snps[,6]
+multigenos=list()
 for (i in 1:length(pops)) {
   tmp<-scan(file.path(pathin,paste0(pops[i],"genofile.",chrno)),what="character",quiet=TRUE)
   tmp<-strsplit(tmp,"")
   allS<-length(tmp)
   N2<-length(tmp[[1]])
-  Y[,5+which(hap.pops==pops[i])]=matrix(sapply(tmp, as.double), N2, allS)
+  tmpfilename<-file.path(pathin,paste0(pops[i],"genofile.",chrno))
+  tmp<-scan(tmpfilename,what="character",quiet=TRUE,nlines=1)
+  N2<-nchar(tmp)
+  multigenos[[i]]=laf_open_fwf(tmpfilename, column_widths=rep(1,N2),column_types=rep("character",N2))
+  Y[,5+which(hap.pops==pops[i])]=matrix(sapply(multigenos[[i]][,], as.double), allS, N2)
 }
 write.table(Y,file=file.path(pathout,hapsfile),sep=" ", quote=FALSE, row.names=FALSE, col.names=FALSE)
 
